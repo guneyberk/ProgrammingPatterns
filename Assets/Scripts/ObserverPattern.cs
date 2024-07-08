a76ae9fc6bcbbe108b77ce24d5f216320085ae8c
berk <guneybrk95@gmail.com>
2024-07-09 00:46:15 +0300

    Creating Project

--- Assets/Scripts/ObserverPattern.cs
+++ Assets/Scripts/ObserverPattern.cs
@@ -7,7 +7,7 @@ public class ObserverPattern : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("here");
     }
 
     // Update is called once per frame
